9d5ee328732ba0879480ba040900edaa9153d3c7
Maxime-Jo <78235958+Maxime-Jo@users.noreply.github.com>
2021-09-28 19:23:30 -0400

    Update agent.py

--- Agent/agent.py
+++ Agent/agent.py
@@ -17,7 +17,7 @@ class Agent:
     def __init__(self, env, exp_buffer):                     # env & exp_buffer
         self.env = env                                       
         self.exp_buffer = exp_buffer
-        self._reset(env)
+        self._reset(self.env)
 
     def _reset(self, env):
         self.state = env.reset()
